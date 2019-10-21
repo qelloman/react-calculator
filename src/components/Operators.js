11e34d7944083f71c0c5c4b232dff1e153f45bb7
Doyun Kim <doyunkm@gmail.com>
2019-10-20 21:44:35 -0700

    click effect

--- src/components/Operators.js
+++ src/components/Operators.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 
 export class Operators extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            clicked: false
+        }
+    }
     click = (e) => {
-        this.props.click(this.props.op)    
+        this.props.click(this.props.op)
+        this.setState( {
+            clicked: true
+        })
+        
     }
     render() {
-        let className = 'btn btn-operator btn-small'
+        let className = 'btn btn-small'
+        if (this.props.op === this.props.lastOp) {
+            className += ' btn-operator-clicked'
+        } else {
+            className += ' btn-operator'
+        }
         return (
             <button className={className} onClick={this.click}> {opSymbols[this.props.op]} </button>
         )
